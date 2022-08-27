718fdb40612ff26c0e08fd062f956192c8abc510
Forest Guhareza <arza.3d@gmail.com>
2022-08-28 06:54:33 +0700

    update assist.js

--- assist.js
+++ assist.js
@@ -94,3 +94,121 @@ function changeTagForDisplay(myText) {
 
   return newText;
 }
+
+    //================
+    // CUSTOM FUNCTION
+    //================
+
+    function makeTextareaSection(myText) {
+      return '<h3>' + myText + `</h3>
+      <section>
+      <textarea>
+      </textarea>
+      </section>`;
+    }
+
+    function makeTable(myText) {
+      const myArray = myText.split('\n');
+      let myTable = '<table>\n';
+      let myTbody = '<tbody>';
+      for (let i = 0; i < myArray.length; i++) {
+        const myArray2 = myArray[i].split(',');
+
+        myTbody += '\n <tr>';
+
+        for (let j = 0; j < myArray2.length; j++) {
+          myTbody += '\n  <td>' + myArray2[j] + '</td>';
+        }
+
+        myTbody += '\n </tr>';
+
+      }
+      myTbody += '\n</tbody>';
+      myTable += myTbody + '\n</table>';
+
+      return myTable;
+    }
+
+    function makeH3Section(myText) {
+      let myArray = myText.split('\n');
+      let myNewText = "";
+      for (let i = 0; i < myArray.length; i++) {
+
+        if (myArray[i].length == 0) {
+          break;
+        }
+        myNewText += '<h3>' + myArray[i] + '</h3>'
+        +'\n<section>\n</section>\n\n';
+      }
+      return myNewText;
+    }
+
+    function makeLoop(myText) {
+      let myLoop = '';
+      let myArray = myText;
+
+      myLoop = 'for (let i = 0; i < '
+      + myArray + '.length; i++) '
+      + '{\n\n'
+      + '}'
+      return myLoop;
+    }
+
+    function deleteExtraSpaces(myText) {
+      return myText.replace(/\s+/g, ' ').trim();
+    }
+
+    function replaceSpaceWithStrip(myText) {
+
+      myText = deleteExtraSpaces(myText);
+      myText = myText.replace(/ /g, '-');
+      return myText;
+    }
+
+    function makeMozillaDevCssLink(myText) {
+      let myCssProperty = replaceSpaceWithStrip(myText);
+      let myNewAnchor = '<a href="';
+      let myLink = 'https://developer.mozilla.org/en-US/docs/Web/CSS/';
+      myNewAnchor += myLink + myCssProperty
+      myNewAnchor += '" >\n';
+      myNewAnchor += myCssProperty;
+      myNewAnchor += '\n</a>'
+      return myNewAnchor;
+    }
+
+    function makeCSSSection(myText) {
+      let myArray = myText.split('\n');
+      let myNewText = '';
+      for (let i = 0; i < myArray.length; i++) {
+        myArray[i] = replaceSpaceWithStrip(myArray[i]);
+        myArray[i] = makeH3Section(myArray[i]);
+        myNewText += myArray[i];
+      }
+      return myNewText;
+    }
+    
+    function makeCSSPropertiesTable(myText) {
+      myText = replaceSpaceWithStrip(myText);
+      myText = makeTable(myText);
+      
+      return myText;
+    }
+    
+    function getW3SchoolProperties() {
+      
+    }
+
+    const inputFunctions =
+    [
+      makeTextareaSection,
+      changeTagForDisplay,
+      makeTable,
+      makeH3Section,
+      makeLoop,
+
+      //css doc assist
+      replaceSpaceWithStrip,
+      makeMozillaDevCssLink,
+      makeCSSSection,
+      makeCSSPropertiesTable
+    ];
