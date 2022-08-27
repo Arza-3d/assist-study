361f8e37d6d8432edfa0524a966ab0a0f37e58c2
Forest Guhareza <arza.3d@gmail.com>
2022-08-28 06:48:42 +0700

    update assist.js

--- assist.js
+++ assist.js
@@ -1,3 +1,32 @@
-function test1() {
-  alert("connected");
-}
+
+
+    //================
+    // Global Function
+    //================
+
+    function getInput(myCount) {
+      return document.getElementById("input" + myCount);
+    }
+
+    function getInputValue(myCount) {
+      return getInput(myCount).value;
+    }
+
+    function getOutput(myCount) {
+      return document.getElementById("output" + myCount);
+    }
+
+    function getRealOutput(myCount) {
+      return document.getElementById("realOutput" + myCount);
+    }
+
+    function getRealOutputValue(myCount) {
+      return getRealOutput(myCount).value;
+    }
+
+    function setRealOutput(myCount, newText) {
+      getRealOutput(myCount).value = newText;
+      getRealOutput(myCount).innerHTML = newText;
+
+    }
+  
