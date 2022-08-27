62cabdaba49ae405d71e874cf7fdad4193c665fb
Forest Guhareza <arza.3d@gmail.com>
2022-08-28 06:51:06 +0700

    update assist.js

--- assist.js
+++ assist.js
@@ -1,32 +1,67 @@
+//================
+// Global Function
+//================
+
+function getInput(myCount) {
+  return document.getElementById("input" + myCount);
+}
+
+function getInputValue(myCount) {
+  return getInput(myCount).value;
+}
+
+function getOutput(myCount) {
+  return document.getElementById("output" + myCount);
+}
+
+function getRealOutput(myCount) {
+  return document.getElementById("realOutput" + myCount);
+}
+
+function getRealOutputValue(myCount) {
+  return getRealOutput(myCount).value;
+}
+
+function setRealOutput(myCount, newText) {
+  getRealOutput(myCount).value = newText;
+  getRealOutput(myCount).innerHTML = newText;
+
+}
 
 
-    //================
-    // Global Function
-    //================
+//=====
+//EVENT
+//=====
 
-    function getInput(myCount) {
-      return document.getElementById("input" + myCount);
-    }
+function copyText() {
 
-    function getInputValue(myCount) {
-      return getInput(myCount).value;
-    }
+  let myCount = this.dataset.count;
+  let myRealOutput = document.getElementById("realOutput" + myCount);
 
-    function getOutput(myCount) {
-      return document.getElementById("output" + myCount);
-    }
+  // for mobile
+  myRealOutput.style.display = "block";
+  myRealOutput.select();
+  myRealOutput.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  myRealOutput.style.display = "none";
 
-    function getRealOutput(myCount) {
-      return document.getElementById("realOutput" + myCount);
-    }
+  // for PC
+  navigator.clipboard.writeText(myRealOutput.innerHTML);
+}
 
-    function getRealOutputValue(myCount) {
-      return getRealOutput(myCount).value;
-    }
+function inputIn() {
 
-    function setRealOutput(myCount, newText) {
-      getRealOutput(myCount).value = newText;
-      getRealOutput(myCount).innerHTML = newText;
+  let myCount = this.dataset.count;
 
-    }
-  
+  let inputText = getInputValue(myCount);
+
+  let newText = inputFunctions[myCount](inputText);
+
+  // save input function output
+  setRealOutput(myCount, newText);
+
+  // display function output
+  let displayedText = changeTagForDisplay(newText);
+
+  getOutput(myCount).innerHTML = displayedText;
+}
