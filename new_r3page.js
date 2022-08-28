6897ab4b844d084fabb86557af0b3deb5e775da4
Forest Guhareza <arza.3d@gmail.com>
2022-08-28 07:07:46 +0700

    update new_r3page.js

---
+++ new_r3page.js
@@ -0,0 +1,152 @@
+let newR3Page = `
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://image.ibb.co/nqbdHJ/titlelogo.png" rel="icon">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <link href="https://raw.githack.com/Arza-3d/ar3.js/master/stylesheets/ar3.min.css" rel="stylesheet" type="text/css">
+
+    <title>belajar SQL</title>
+    <style>
+        .filter-r3 input[type="text"] {
+            max-width: 10px;
+            border-radius: 5px;
+        }
+        .filter-r3 input[type="text"]:focus {
+            max-width: none;
+        }
+    </style>
+</head>
+<body
+    data-repo-r3="https://github.com/Arza-3d/doc-py-string"
+    data-headers-r3="> div > h2| > div > h3| div > section > h4"
+    >
+    <script src="https://raw.githack.com/Arza-3d/ar3.js/master/ar3header.js"></script>
+    <header>
+        <h1>belajar SQL</h1>
+        <h4>rangkuman SQL</h4>
+        <table>
+            <tr>
+                <td>project repo</td>
+                <td><a href="https://github.com/Arza-3d/doc-sql">https://github.com/Arza-3d/doc-sql</a></td>
+            </tr>
+        </table>
+        <p>
+            Catatan belajar SQL
+        </p>
+    </header>
+    <main class="aside-nav-target-r3">
+
+        <div data-title-r3="database">
+            <h2>database</h2>
+            <br />
+
+            <code><b>init</b> &gt; <b>set</b> &gt; <b>get</b> &gt; <b>drop</b></code>
+
+            <h3>init</h3>
+            <section>
+
+            </section>
+
+            <h3>set</h3>
+            <section>
+
+                <h4>create</h4>
+                <code><span class="red-r3">CREATE DATABASE</span> <i>MYDB1</i></code>
+                <h4>import</h4>
+                <p>
+                    import database
+                </p>
+
+
+            </section>
+
+            <h3>get</h3>
+            <section>
+                fetch database
+
+            </section>
+
+            <h3>drop</h3>
+            <section>
+                <code><span class="red-r3">DROP DATABASE</span> <i>MYDB1</i></code>
+            </section>
+
+        </div>
+        <div data-title-r3="dll">
+            <h2>dll</h2>
+            <h3>comment</h3>
+            <section>
+                <code>-- <i>komen aq</i></code>
+            </section>
+
+            <h3>multi</h3>
+            <section>
+                <code>//</code>
+            </section>
+            <h3>installer</h3>
+            <section>
+                <a href="https://www.google.com/search?q=installing+sql+server+2019+step+by+step&oq=installing+sql&aqs=chrome.0.0i512j69i57j0i20i263i512j0i512l6.7606j0j7">how to install</a>
+                <h4>link</h4>
+                <table border="0">
+                    <thead>
+                        <tr>
+                            <th>dev</th>
+                            <th>link</th>
+                        </tr>
+                        
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        
+                    </tbody>
+                </table>
+                <h4>how</h4>
+            </section>
+            
+            <h3>doc</h3>
+            <section>
+                
+            </section>
+            
+        </div>
+        <div data-title-r3="format">
+
+            <h2>string format</h2>
+
+            <h3>string format</h3>
+            <section>
+
+            </section>
+
+        </div>
+        <div data-title-r3="property">
+            <h2>properti string</h2>
+            <h3>tabel properti string</h3>
+
+            <section>
+                <a href="https://docs.google.com/spreadsheets/d/1qjAhyzcZc16nu4uLi73FfoyNGJ_XOkJU61LXz8stRLA/edit?usp=sharing">googlesheet properti string</a>
+
+                <table data-JSON-full-r3="pyStringFunction" class="filter-r3">
+
+                </table>
+
+            </section>
+
+        </div>
+    </main>
+    <script src="https://raw.githack.com/Arza-3d/ar3.js/master/ar3.min.js"></script>
+
+    <script src="database/python-string_properties.json"></script>
+    <script src="https://raw.githack.com/Arza-3d/ar3.js/master/modular/table-JSON/ar3-JSON-table-full-1.js"></script>
+    <script src="https://raw.githack.com/Arza-3d/ar3.js/master/modular/table-filter/ar3-table-filter-1.js"></script>
+
+    
+</body>
+</html>
+`
