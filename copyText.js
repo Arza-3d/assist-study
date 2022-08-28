afec9847116f4de8c3fe36712d44570302957b88
Forest Guhareza <arza.3d@gmail.com>
2022-08-28 17:10:15 +0700

    update copyText.js

--- copyText.js
+++ copyText.js
@@ -1,6 +1,9 @@
 function copyText(myText) {
   let docBody = document.body;
   let tempText = document.createElement('textarea');
+  tempText.id = 'myTemporaryText';
+  
+  tempText.setAttribute()
   
   //create document
 }
