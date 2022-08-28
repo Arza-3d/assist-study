607c9083b429ea3fde59eae7edbccb250ba6506d
Forest Guhareza <arza.3d@gmail.com>
2022-08-28 07:19:50 +0700

    update new_r3page.js

--- new_r3page.js
+++ new_r3page.js
@@ -1,55 +1,5 @@
-let pageTitle = '',
-repoTitle = '';
-
-let newR3Page = `
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link href="https://image.ibb.co/nqbdHJ/titlelogo.png" rel="icon">
-<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-<link href="https://raw.githack.com/Arza-3d/ar3.js/master/stylesheets/ar3.min.css" rel="stylesheet" type="text/css">
-
-<title>` + pageTitle + `</title>
-</head>
-<body
-data-repo-r3="https://github.com/Arza-3d/` + repoTitle + `"
-data-headers-r3="> div > h2| > div > h3| div > section > h4"
->
-<script src="https://raw.githack.com/Arza-3d/ar3.js/master/ar3header.js"></script>
-<header>
-<h1>.</h1>
-<h4>.</h4>
-<table>
-<tr>
-<td>project repo</td>
-<td><a href="https://github.com/Arza-3d/doc-sql">https://github.com/Arza-3d/doc-sql</a></td>
-</tr>
-</table>
-<p>
-lorem ipsum
-</p>
-</header>
-<main class="aside-nav-target-r3">
-
-<div data-title-r3=".">
-<h2>.</h2>
-<h3>.</h3>
-
-<section>
-
-</section>
-
-</div>
-</main>
-<script src="https://raw.githack.com/Arza-3d/ar3.js/master/ar3.min.js"></script>
-</body>
-</html>
-`
 
 function makeNewR3Page(myPageTitle, myRepoTitle) {
-
 
   let myNewR3Page = `
   <!DOCTYPE html>
@@ -61,10 +11,10 @@ function makeNewR3Page(myPageTitle, myRepoTitle) {
   <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
   <link href="https://raw.githack.com/Arza-3d/ar3.js/master/stylesheets/ar3.min.css" rel="stylesheet" type="text/css">
 
-  <title>` + pageTitle + `</title>
+  <title>` + myPageTitle + `</title>
   </head>
   <body
-  data-repo-r3="https://github.com/Arza-3d/` + repoTitle + `"
+  data-repo-r3="https://github.com/Arza-3d/` + myRepoTitle + `"
   data-headers-r3="> div > h2| > div > h3| div > section > h4"
   >
   <script src="https://raw.githack.com/Arza-3d/ar3.js/master/ar3header.js"></script>
