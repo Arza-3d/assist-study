1e47b1574bcec5786715c22699c5c87da630a31e
Forest Guhareza <arza.3d@gmail.com>
2022-08-28 17:14:17 +0700

    update copyText.js

--- copyText.js
+++ copyText.js
@@ -9,5 +9,11 @@ function copyText(myText) {
   
   tempText.innerHTML = myText;
   tempText.value = myText;
+  
+  tempText.style.display = "block";
+  myRealOutput.select();
+  myRealOutput.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  myRealOutput.style.display = "none";
   //create document
 }
