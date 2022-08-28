449b41e36efcc615c91cbc836bf22d27cc2375e2
Forest Guhareza <arza.3d@gmail.com>
2022-08-28 07:11:03 +0700

    update new_r3page.js

--- new_r3page.js
+++ new_r3page.js
@@ -8,16 +8,7 @@ let newR3Page = `
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <link href="https://raw.githack.com/Arza-3d/ar3.js/master/stylesheets/ar3.min.css" rel="stylesheet" type="text/css">
 
-    <title>belajar SQL</title>
-    <style>
-        .filter-r3 input[type="text"] {
-            max-width: 10px;
-            border-radius: 5px;
-        }
-        .filter-r3 input[type="text"]:focus {
-            max-width: none;
-        }
-    </style>
+    <title>.</title>
 </head>
 <body
     data-repo-r3="https://github.com/Arza-3d/doc-py-string"
@@ -39,44 +30,6 @@ let newR3Page = `
     </header>
     <main class="aside-nav-target-r3">
 
-        <div data-title-r3="database">
-            <h2>database</h2>
-            <br />
-
-            <code><b>init</b> &gt; <b>set</b> &gt; <b>get</b> &gt; <b>drop</b></code>
-
-            <h3>init</h3>
-            <section>
-
-            </section>
-
-            <h3>set</h3>
-            <section>
-
-                <h4>create</h4>
-                <code><span class="red-r3">CREATE DATABASE</span> <i>MYDB1</i></code>
-                <h4>import</h4>
-                <p>
-                    import database
-                </p>
-
-
-            </section>
-
-            <h3>get</h3>
-            <section>
-                fetch database
-
-            </section>
-
-            <h3>drop</h3>
-            <section>
-                <code><span class="red-r3">DROP DATABASE</span> <i>MYDB1</i></code>
-            </section>
-
-        </div>
-        
-        
         <div data-title-r3=".">
             <h2>.</h2>
             <h3>.</h3>
